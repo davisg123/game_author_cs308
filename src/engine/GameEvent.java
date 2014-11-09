1e2e692b3b17747c8329ad555a799f6677a8b943
William Chang <wkc10@duke.edu>
2014-11-09 17:48:39 -0500

    GameEvent class. Also a test commit to my branch.

---
+++ src/engine/GameEvent.java
@@ -0,0 +1,12 @@
+package engine;
+
+/**
+ * This class will define the handlers for the action classes and 
+ * execute the corresponding sequence of updates based on actions.
+ * @author Will Chang
+ *
+ */
+
+public class GameEvent {
+    
+}
