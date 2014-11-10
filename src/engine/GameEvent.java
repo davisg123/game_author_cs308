b0bcf5f96480b787bfad05083900b075f9906ca6
William Chang <wkc10@duke.edu>
2014-11-09 22:31:19 -0500

    Small updates, make GameEvent into Interface, waiting to merge and make packages.

--- src/engine/GameEvent.java
+++ src/engine/GameEvent.java
@@ -3,10 +3,37 @@ package engine;
 /**
  * This class will define the handlers for the action classes and 
  * execute the corresponding sequence of updates based on actions.
+ * 
+ * Can call methods to change Sprite state, Level state, CameraView state...
+ * 
+ * 
  * @author Will Chang
  *
  */
 
-public class GameEvent {
+
+/*
+ * Types of events
+ *      - Collision events
+ *      - state changes:
+ *              - health
+ *              - location 
+ *              - orientation
+ *              - velocity
+ *              - images.../display and visibility
+ *      - Game Progression Events
+ *              - Game Start
+ *              - Game End
+ *                      - Win/lose
+ *              - Levels
+ *      - Camera changes...
+ */
+
+public interface GameEvent {
+    /**
+     * Will execute this Event resulting from applying an Action.
+     * @param action
+     */
+    public abstract void execute ();
     
 }
