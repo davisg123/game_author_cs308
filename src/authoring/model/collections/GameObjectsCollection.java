252d8ade7461744ca3eff7be5d93b19789ca5ebf
Kevin <siyuan.li@duke.edu>
2014-11-23 14:26:16 -0500

    added collections to hashmap. not yet finished

--- src/authoring/model/collections/GameObjectsCollection.java
+++ src/authoring/model/collections/GameObjectsCollection.java
@@ -1,9 +1,6 @@
 package authoring.model.collections;
 
 
-import java.util.ArrayList;
-import java.util.List;
-
 import engine.gameObject.GameObject;
 
 public class GameObjectsCollection extends GeneralCollection<GameObject>{
