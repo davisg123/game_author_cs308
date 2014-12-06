f2c74cada8d945d573f001a48d68782a2dabc3fb
jarjun <arjun.jain@duke.edu>
2014-12-05 23:09:38 -0500

    minor refactoring

--- src/authoring/view/levelview/SingleLevelView.java
+++ src/authoring/view/levelview/SingleLevelView.java
@@ -92,7 +92,7 @@ public class SingleLevelView extends Pane implements Observer {
 		
 		g.makeGraphic(myGameLocation, gameObject.getWidth(), gameObject.getHeight(), gameObject.getRotation());
 		
-		this.moveSpriteOnLevel(g, x, y);
+		this.moveGameObjectToLevel(g, x, y);
 		this.getChildren().add(g);
 
 	}
@@ -112,7 +112,7 @@ public class SingleLevelView extends Pane implements Observer {
 		return myID;
 	}
 
-	public void moveSpriteOnLevel(Graphic g, double x, double y) {
+	public void moveGameObjectToLevel(Graphic g, double x, double y) {
 		g.setLayoutX(x + OBJECT_X_OFFSET);
 		g.setLayoutY(y + OBJECT_Y_OFFSET);
 	}
