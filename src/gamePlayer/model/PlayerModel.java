29b96120725eb413dd617ce4004839da52ffb11f
Abhishek Balakrishnan <abhishek.bend@gmail.com>
2014-12-07 00:35:40 -0500

    My program doesn't work so hopefully these files I changed work on Safkat's machine

--- src/gamePlayer/model/PlayerModel.java
+++ src/gamePlayer/model/PlayerModel.java
@@ -1,11 +1,15 @@
 package gamePlayer.model;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 import data.DataManager;
 import application.SplashScreen;
 import authoring.model.GameData;
+import authoring.model.collections.ConditionsCollection;
 import engine.GameManager;
+import engine.conditions.ButtonCondition;
+import engine.conditions.Condition;
 import gamePlayer.view.PlayerView;
 
 public class PlayerModel {
@@ -13,7 +17,8 @@ public class PlayerModel {
 	private GameData myGameData;
 	private DataManager myManager; 
 	private PlayerView myPlayerView;
-	private GameManager myGameManager; 
+	private GameManager myGameManager;
+	private ConditionsCollection myButtonConditions;
 	
 	public PlayerModel() throws IOException {
 		myPlayerView = new PlayerView(this);
@@ -24,21 +29,37 @@ public class PlayerModel {
 		myPlayerView.initialize();
 	}
 	
-	public void loadGameFile(){		
+	public void loadGameFile() {
 		myGameData = myManager.readGameFile("fd_final.json");
 		myGameManager = new GameManager(myGameData.getConditions(), myGameData.getGameObjects(), myGameData.getLevels(), myPlayerView.getGroup());
 		myGameManager.initialize();
+		
+		extractButtonConditions();
 	}
 	
 	public void exitFromGame(){
-		//myGameManager.clear()
 		myPlayerView.close();
 		SplashScreen screen = new SplashScreen();
-		screen.show();	
-		}
+		screen.show();
+	}
 
 	public void pauseGame(){
 		myGameManager.togglePause(); 
+	}
+	
+	private void extractButtonConditions() {
+		myButtonConditions = new ConditionsCollection();
+		ConditionsCollection conditions = myGameData.getConditions();
+		for (Iterator<Condition> conditionIterator = conditions.iterator(); conditionIterator.hasNext();) {
+			Condition condition = conditionIterator.next();
+			if(condition instanceof ButtonCondition) {
+				myButtonConditions.add(condition);
+			}
+		}
+	}
+	
+	public ConditionsCollection getButtonConditions() {
+		return myButtonConditions;
 	}
 	
 }
--- src/gamePlayer/view/KeyboardView.java
+++ src/gamePlayer/view/KeyboardView.java
@@ -1,15 +1,21 @@
 package gamePlayer.view;
 
-import javafx.application.Application;
+import java.util.Iterator;
+import java.util.Map;
+
+import engine.conditions.ButtonCondition;
+import engine.conditions.Condition;
+import authoring.model.collections.ConditionsCollection;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class KeyboardView extends Application {
+public class KeyboardView {
 
-	public static final String[] keys = {"`", "1", "2", "3", "4", "5",
+	private static final String[] KEYS = {"`", "1", "2", "3", "4", "5",
 		"6", "7", "8", "9", "0", "-", "=", "<----", "tab", "Q", "W",
 		"E", "R", "T", "Y", "U", "I", "O", "P", "[", "]", "\\", "caps lock",
 		"A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'", "enter", "", "shift", 
@@ -18,34 +24,55 @@ public class KeyboardView extends Application {
 
 	private static final double WIDTH = 950;
 	private static final double HEIGHT = 300;
+	private GridPane myGrid;
+	private Stage myStage;
+	private ConditionsCollection myButtonConditions;
+	private Map<Condition, String> myButtonConditionsMap;
+	private ComboBox<String> myFunctionComboBox;
 	
-	public static void main(String[] args)
-	{
-		launch(args);
-	}
-
-	@Override
-	public void start(Stage s) throws Exception {
-		Stage myStage = new Stage();
+	public KeyboardView(ConditionsCollection buttonConditions) {
+		myStage = new Stage();
 		myStage.setTitle("Vooga Salad Bits Please Keyboard Mapping Utility");
-		GridPane myPane = new GridPane();
-		Scene myScene = new Scene(myPane, WIDTH, HEIGHT);
+		myGrid = new GridPane();
+		Scene myScene = new Scene(myGrid, WIDTH, HEIGHT);
 //		myScene.getStylesheets().add(getClass().getResource("layoutstyles.css").toExternalForm());
 		myStage.setScene(myScene);
+		myButtonConditions = buttonConditions;
+		myFunctionComboBox = comboBoxForButton();
+	}
+	
+	public void buildKeyboard() {
 		int num = 0;
 		for(int i = 0; i < 5; i++)
 		{
 			for(int j = 0; j < 14; j++)
 			{
-				StackPane sp = new StackPane();
-				Button b = new Button(keys[num]);
-				b.setPrefSize(70, 70);
-				sp.getChildren().add(b);
-				myPane.add(sp, j, i);
-				num++;				
+				num = buildButton(num, i, j);
 			}
 		}
 		myStage.show();
 	}
 
+	private int buildButton(int num, int i, int j) {
+		StackPane sp = new StackPane();
+		Button button = new Button(KEYS[num]);
+		button.setPrefSize(70, 70);
+		button.setDisable(KEYS[num] == "");
+		button.setOnAction((event) -> {
+			KeyMapForm keyMapForm = new KeyMapForm(button.getText(), myFunctionComboBox);
+		});
+		sp.getChildren().add(button);
+		myGrid.add(sp, j, i);
+		num++;
+		return num;
+	}
+	
+	private ComboBox comboBoxForButton() {
+		ComboBox<String> functionCombos = new ComboBox<String>();
+		for (Iterator<Condition> conditionIterator = myButtonConditions.iterator(); conditionIterator.hasNext();) {
+			ButtonCondition condition = (ButtonCondition) conditionIterator.next();
+			functionCombos.getItems().add(condition.getKeyIdentifier());
+		}
+		return functionCombos;
+	}
 }
