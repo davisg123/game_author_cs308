f8e4d3870a6e3c722a9348c4ecd0d27e8d805204
elichtenberg93 <erl15@duke.edu>
2014-12-08 04:33:15 -0500

    ErrorPopUp can show StackTrace; other things added

--- src/errorsAndExceptions/ErrorPopUp.java
+++ src/errorsAndExceptions/ErrorPopUp.java
@@ -1,10 +1,17 @@
 package errorsAndExceptions;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -19,27 +26,31 @@ import javafx.stage.Stage;
  */
 public class ErrorPopUp {
 	
-	private static final int POP_UP_HEIGHT = 100;
+	private static final int POP_UP_HEIGHT = 150;
 	private static final int POP_UP_WIDTH = 300;
 	private static final int TEXT_Y_COORD = POP_UP_HEIGHT/2;
 	private static final int TEXT_X_COORD = POP_UP_WIDTH/4;
-	private static final int BUTTON_Y_COORD = POP_UP_HEIGHT*5/8;
+	private static final int BUTTON_Y_COORD = POP_UP_HEIGHT*4/8;
 	private static final int BUTTON_X_COORD = POP_UP_WIDTH/2;
 	private static final String ERROR_TITLE = "User Error";
 	private static final String BUTTON_TEXT = "OK";
 	private Group root;
 	private Stage popUpStage;
 	
+	private Exception myException;
+	
 	/**
 	 * Constructor.
 	 */
-	public ErrorPopUp() {
+	public ErrorPopUp(Exception e) {
 		popUpStage = new Stage();
 		popUpStage.setTitle(ERROR_TITLE);
 		root = new Group();
 		Scene scene = new Scene(root, POP_UP_WIDTH, POP_UP_HEIGHT, Color.LIGHTGRAY);
 		popUpStage.setScene(scene);
+		myException = e;
 	}
+	
 	
 	/**
 	 * EventHandler to close the PopUp window.
@@ -60,8 +71,10 @@ public class ErrorPopUp {
 	 */
 	public void display(String errorMessage, boolean throwException) throws VoogaException{
 		root.getChildren().clear();
+		VBox box = new VBox();
+		root.getChildren().add(box);
 		Text errorText = new Text(TEXT_X_COORD, TEXT_Y_COORD, errorMessage);
-		root.getChildren().add(errorText);
+		box.getChildren().add(errorText);
 		
 		Button b = new Button();
 		b.setText(BUTTON_TEXT);
@@ -69,12 +82,48 @@ public class ErrorPopUp {
 		b.setLayoutY(BUTTON_Y_COORD);
 		root.getChildren().add(b);
 		b.setOnAction(closePopUp);
+		
+		createErrorButton();
+				
 		popUpStage.show();
 		
 		if(throwException)
 		{
 			throw new VoogaException(errorMessage);
 		}
+	}
+
+	private void createErrorButton() {
+		// TODO Auto-generated method stub
+		Button b = new Button();
+		b.setText("View Error Trace");
+		b.setLayoutX(BUTTON_X_COORD-40);
+		b.setLayoutY(BUTTON_Y_COORD+40);
+		b.setOnAction(handle -> viewErrorTrace());
+		root.getChildren().add(b);
+	}
+	
+	private void viewErrorTrace() {
+		Stage errorTraceStage = new Stage();
+		errorTraceStage.setX(popUpStage.getX()*1.75);
+		errorTraceStage.setY(popUpStage.getY()*.75);
+		VBox errorBox = new VBox();
+		ScrollPane sp = new ScrollPane();
+		Label errorTrace = new Label(getStackTrace());
+		sp.setContent(errorTrace);
+		errorBox.getChildren().add(sp);
+		errorBox.setVgrow(sp, Priority.ALWAYS);
+		Scene scene = new Scene(errorBox, POP_UP_WIDTH, POP_UP_HEIGHT*3, Color.LIGHTGRAY);
+		errorTraceStage.setScene(scene);
+		errorTraceStage.show();
+	}
+
+	private String getStackTrace() {
+		// TODO Auto-generated method stub
+		StringWriter sw = new StringWriter();
+		PrintWriter pw = new PrintWriter(sw);
+		myException.printStackTrace(pw);
+		return sw.toString();
 	}
 
 }
