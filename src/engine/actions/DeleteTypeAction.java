b9394d7eaa463914909ba0215d475739d2edbd34
Abhishek Balakrishnan <abhishek.bend@gmail.com>
2014-12-08 05:05:44 -0500

    empty delete action... composition?

--- src/engine/actions/DeleteTypeAction.java
+++ src/engine/actions/DeleteTypeAction.java
@@ -3,38 +3,36 @@ package engine.actions;
 import authoring.model.collections.GameObjectsCollection;
 import engine.GameManager;
 import engine.gameObject.GameObject;
-import engine.gameObject.Identifiable;
 import engine.gameObject.Identifier;
 
-public class DeleteTypeAction implements Action, Initializable{
+/**
+ * 
+ * @author Shreyas B
+ * @author Abhishek B
+ *
+ */
+public class DeleteTypeAction implements Action, Initializable {
 
 	private String myType;
-	private GameObjectsCollection myGameObjects; 
-	
-	public DeleteTypeAction(String type, Identifier... ids){
-		myType=type;
+	private GameObjectsCollection myGameObjects;
+
+	public DeleteTypeAction(String type, Identifier... ids) {
+		myType = type;
 	}
-	
+
 	@Override
 	public void initialize(GameManager manager) {
-		
-		myGameObjects = manager.getLevelManager().getCurrentLevel().getGameObjectsCollection();
+		myGameObjects = manager.getLevelManager().getCurrentLevel()
+				.getGameObjectsCollection();
 	}
 
 	@Override
 	public void execute() {
- 
-		for (GameObject object: myGameObjects){
-			if (object.getIdentifier().getType().equals(myType)){
+
+		for (GameObject object : myGameObjects) {
+			if (object.getIdentifier().getType().equals(myType)) {
 				myGameObjects.remove(object);
 			}
 		}
-		
 	}
-
-	
-	
-
-	
-	
 }
