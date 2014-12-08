d44ee81ab4b7a49274868662e0f01bc93711d116
wsvalentine94 <wsv@duke.edu>
2014-12-08 05:11:39 -0500

    bug fix adding actions to conditions

--- src/authoring/view/propertiesview/ConditionProperties.java
+++ src/authoring/view/propertiesview/ConditionProperties.java
@@ -4,36 +4,40 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
+import authoring.eventhandlers.AddActionHandler;
 import authoring.eventhandlers.GameHandler;
 import authoring.view.icons.ConditionIcon;
+import engine.conditions.Condition;
 
-public class ConditionProperties extends Properties{
+public class ConditionProperties extends Properties {
 
 	private GameHandler myHandler;
-	
-	//will refactor pl0X
+	private Condition myCondition;
+
+	// will refactor pl0X
 	public ConditionProperties(Object o, GameHandler gh) {
 		myHandler = gh;
+		myCondition = ((ConditionIcon) o).getCondition();
 		initializeProperties(o);
 	}
 
 	@Override
 	public void initializeProperties(Object o) {
 		ConditionIcon icon = (ConditionIcon) o;
-		
+
 		this.getChildren().clear();
-		
+
 		HBox imageField = new HBox();
 		imageField.getChildren().add(new Text("Name: "));
 		imageField.getChildren().add(new TextField(icon.getName()));
 		this.getChildren().add(imageField);
-		
+
 		Button addAction = new Button();
 		addAction.setText("Add Action");
+		((AddActionHandler)myHandler).setCondition(myCondition);
 		addAction.setOnAction(myHandler);
 		this.getChildren().add(addAction);
-		
-		
+
 	}
-	
+
 }
