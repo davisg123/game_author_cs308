e409f9743da82e56a6a0aefa38372b18dc61b827
Abhishek Balakrishnan <abhishek.bend@gmail.com>
2014-12-08 01:48:52 -0500

    refactoring

--- src/gamePlayer/view/MenuBarController.java
+++ src/gamePlayer/view/MenuBarController.java
@@ -41,11 +41,6 @@ public class MenuBarController implements Initializable{
 	}
 	
 	@FXML
-	private void goHome(final ActionEvent event){
-		myPlayerModel.goHome();
-	}
-	
-	@FXML
 	private void loadGameFile(final ActionEvent event){
 		myPlayerModel.loadGameFile();
 		gameSelector.setDisable(true);
@@ -53,42 +48,13 @@ public class MenuBarController implements Initializable{
 		gamePreferences.setDisable(false);
 	}
 	
-	@FXML 
+	@FXML
 	private void exitFromGame(final ActionEvent event){
 		myPlayerModel.exitFromGame();
 		gameOptions.setDisable(true);
 		gamePreferences.setDisable(true);
 	}
 	
-	@FXML
-	private void loadFromSave(final ActionEvent event) throws CloneNotSupportedException{
-		myPlayerModel.loadProgressFile();
-		gameSelector.setDisable(true);
-		gameOptions.setDisable(false);
-		gamePreferences.setDisable(false);
-	}
-	
-	@FXML
-	private void newGame(final ActionEvent event){
-		myPlayerModel.newGame();
-		gameSelector.setDisable(true);
-		gameOptions.setDisable(false);
-		gamePreferences.setDisable(false);
-	}
-	
-	@FXML
-	private void saveGame(final ActionEvent event){
-		myPlayerModel.saveGame();
-	}
-		
-	@FXML
-	private void quitGame(final ActionEvent event){
-		myPlayerModel.quitGame();
-		gameSelector.setDisable(false);
-		gameOptions.setDisable(true);
-		gamePreferences.setDisable(true);
-	}
-
 	@FXML
 	private void pauseGame(final ActionEvent event){
 		myPlayerModel.pauseGame();
