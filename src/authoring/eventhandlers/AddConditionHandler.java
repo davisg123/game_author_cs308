eb86ad213a6105e399cb81f25e6448a1a05610b2
wsvalentine94 <wsv@duke.edu>
2014-12-08 01:14:02 -0500

    conditions completed, but no actions yet

--- src/authoring/eventhandlers/AddConditionHandler.java
+++ src/authoring/eventhandlers/AddConditionHandler.java
@@ -9,11 +9,13 @@ import java.util.List;
 
 import javafx.event.Event;
 import javafx.event.EventType;
+import authoring.model.collections.ConditionsCollection;
 import authoring.view.wizards.ActionChoiceWizard;
 import authoring.view.wizards.ConditionParameterWizard;
 import authoring.view.wizards.ConditionSelectionWizard;
 import engine.actions.Action;
 import engine.conditions.Condition;
+import engine.gameObject.Identifier;
 
 public class AddConditionHandler implements GameHandler<Event> {
 
@@ -21,7 +23,7 @@ public class AddConditionHandler implements GameHandler<Event> {
 	private static final int WIZARD_WIDTH = 300;
 	private static final int WIZARD_HEIGHT = 600;
 
-	private ConditionSelectionWizard myWizard;
+	private ConditionSelectionWizard mySelectionWizard;
 	private ConditionParameterWizard myCPW;
 	private ActionChoiceWizard myActionChoiceWizard;
 
@@ -29,9 +31,16 @@ public class AddConditionHandler implements GameHandler<Event> {
 	private Parameter[] myParameterTypes;
 	private Class<?> classType;
 	private Constructor myConstructor;
+	
+	private ConditionsCollection myConditionsCollection;
+	
+	public AddConditionHandler(ConditionsCollection conditions){
+		myConditionsCollection = conditions;
+	}
+	
 	@Override
 	public void handle(Event arg0) {
-		myWizard = new ConditionSelectionWizard("New Condition", WIZARD_WIDTH,
+		mySelectionWizard = new ConditionSelectionWizard("New Condition", WIZARD_WIDTH,
 				WIZARD_HEIGHT, event -> createCondition());
 	}
 
@@ -41,7 +50,7 @@ public class AddConditionHandler implements GameHandler<Event> {
 	}
 
 	private void createCondition() {
-		String selected = myWizard.getSelectedCondition();
+		String selected = mySelectionWizard.getSelectedCondition();
 
 		try {
 			classType = Class.forName(CONDITION_PATH_START + selected);
@@ -77,27 +86,21 @@ public class AddConditionHandler implements GameHandler<Event> {
 		for (String s : myCPW.getMap().keySet()) {
 			myInputParameters.add(myCPW.getMap().get(s).getInformation());
 		}
-		myWizard.disableSelection();
-		myWizard.enableActionCreation(event -> addAction());
-		Object[] inputs = convertInputParameters();
-		try {
-			Condition c = (Condition) myConstructor.newInstance(inputs);
-			System.out.println(c.getClass());
-		} catch (Exception e) {
-			System.out.println("Could not construct");
-		}
+		mySelectionWizard.disableSelection();
+		//myWizard.enableActionCreation(event -> addAction());
+		mySelectionWizard.enableFinalize(event -> finishCondition());
 		myCPW.close();
 	}
 
-	private void addAction() {
-		myActionChoiceWizard = new ActionChoiceWizard("Choose Action",
-				WIZARD_WIDTH, WIZARD_HEIGHT, event -> createAction());
-
-	}
-
-	private void createAction() {
-		System.out.println("heyah");
-	}
+//	private void addAction() {
+//		myActionChoiceWizard = new ActionChoiceWizard("Choose Action",
+//				WIZARD_WIDTH, WIZARD_HEIGHT, event -> createAction());
+//
+//	}
+//
+//	private void createAction() {
+//		System.out.println("heyah");
+//	}
 
 	private Object[] convertInputParameters() {
 		List<Object> inputs = new ArrayList<Object>();
@@ -141,6 +144,20 @@ public class AddConditionHandler implements GameHandler<Event> {
 		}
 		return inputs.toArray();
 		
+	}
+	
+	private void finishCondition(){
+		Object[] inputs = convertInputParameters();
+		try {
+			Condition c = (Condition) myConstructor.newInstance(inputs);
+			c.setIdentifier(new Identifier("Condition", mySelectionWizard.getMap().get("name").getInformation()));
+			myConditionsCollection.add(c);
+			mySelectionWizard.close();
+			System.out.println(c.getClass());
+		} catch (Exception e) {
+			System.out.println("Could not construct");
+			e.printStackTrace();
+		}
 	}
 
 }
