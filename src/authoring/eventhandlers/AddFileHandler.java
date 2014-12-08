76fd13316ba7ae28c0e42cc516dcdbfd68b34da0
Kevin <siyuan.li@duke.edu>
2014-12-07 19:26:03 -0500

    refactoring: made addfilehandler abstract class that addimagehandler and addsoundhandler extends

---
+++ src/authoring/eventhandlers/AddFileHandler.java
@@ -0,0 +1,54 @@
+package authoring.eventhandlers;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import javafx.event.Event;
+import javafx.event.EventType;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import authoring.model.collections.GeneralCollection;
+
+public abstract class AddFileHandler implements GameHandler<Event> {
+	protected GeneralCollection myCollection;
+	protected File myGameFile;
+	protected FileChooser myFileChooser;
+	protected String myFileLocation;
+
+	public AddFileHandler(File file) {
+		myGameFile = file;
+		myFileChooser = new FileChooser();
+
+	}
+
+	@Override
+	public void handle(Event arg0) {
+		File imageFile = myFileChooser.showOpenDialog(new Stage());
+		if (imageFile != null) {
+			/*
+			 * try { BufferedImage bi = ImageIO.read(imageFile); File
+			 * newImageFile = new File(myGameFile.getPath() + "/images/" +
+			 * imageFile.getName()); ImageIO.write(bi, "png", newImageFile);
+			 * System.out.println(newImageFile.getPath());
+			 * myImagesCollection.add(newImageFile.getName()); } catch
+			 * (IOException e) { System.out.println("Bad File"); } }
+			 */
+			try {
+				File destFile = new File(myGameFile.getPath() + "/images/"
+						+ imageFile.getName());
+				Files.copy(imageFile.toPath(), destFile.toPath());
+				myCollection.add(destFile.getName());
+			} catch (IOException e) {
+				System.out.println("Bad File");
+			}
+		}
+	}
+
+	@Override
+	public EventType<Event> getEventType() {
+		// TODO Auto-generated method stub
+		return Event.ANY;
+	}
+
+}
