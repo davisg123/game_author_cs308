cee5371f53f179a2134c8d64265d3a18ad0ef9e0
Jonno Schmidt <jonathan.schmidt@duke.edu>
2014-12-14 09:10:59 +0000

    Added Readme

--- Doxygen/html/search/variables_e.js
+++
@@ -1,9 +0,0 @@
-var searchData=
-[
-  ['physicsproperties',['physicsProperties',['../da/d8d/classauthoring_1_1view_1_1propertiesview_1_1_game_object_properties.html#add1fa2b04b7ff8b75e2917606978eb64',1,'authoring::view::propertiesview::GameObjectProperties']]],
-  ['pop_5fup_5fheight',['POP_UP_HEIGHT',['../de/d50/classerrors_and_exceptions_1_1_error_pop_up.html#ac1a61b3ce4b81ec8be9a5637ddaaa455',1,'errorsAndExceptions::ErrorPopUp']]],
-  ['pop_5fup_5fwidth',['POP_UP_WIDTH',['../de/d50/classerrors_and_exceptions_1_1_error_pop_up.html#afef407d8b807c71a1ec89fb175f32649',1,'errorsAndExceptions::ErrorPopUp']]],
-  ['popupstage',['popUpStage',['../de/d50/classerrors_and_exceptions_1_1_error_pop_up.html#a9ba253557c44c45a3208a7fa3169b7eb',1,'errorsAndExceptions::ErrorPopUp']]],
-  ['progress_5ffolder_5fname',['PROGRESS_FOLDER_NAME',['../d1/d95/classdata_1_1_data_manager.html#a46e6c57dd5507f94e922a90778283503',1,'data::DataManager']]],
-  ['progressfilename',['progressFileName',['../d2/dc9/classgame_player_1_1model_1_1_player_model.html#a2e3c71aa01a9143f70cf3d226a4cd935',1,'gamePlayer::model::PlayerModel']]]
-];
