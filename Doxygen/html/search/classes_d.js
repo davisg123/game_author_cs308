1b74d2c18584075a0ed12a47c5532cba459ef163
Jonno Schmidt <jonathan.schmidt@duke.edu>
2014-12-14 05:09:05 +0000

    Added Readme

--- Doxygen/html/search/classes_d.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['observable',['Observable',['../d3/d8d/classdata_1_1_observable.html',1,'data']]],
-  ['observer',['Observer',['../d2/d4f/interfacedata_1_1_observer.html',1,'data']]]
-];
