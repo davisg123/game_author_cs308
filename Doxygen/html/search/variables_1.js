c935db3faf98b9223e7b8c8d231118f4f230d683
Jonno Schmidt <jonathan.schmidt@duke.edu>
2014-12-14 10:21:07 +0000

    Added Readme

--- Doxygen/html/search/variables_1.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['background',['BACKGROUND',['../df/de5/classengine_1_1render_1_1_game_object_renderer.html#a6b841147fc9aabc09767fc9259f4ad38',1,'engine::render::GameObjectRenderer']]],
-  ['booleanproperties',['booleanProperties',['../da/d8d/classauthoring_1_1view_1_1propertiesview_1_1_game_object_properties.html#a5a58ebcd973e87ef0343d1ba7e69a8f6',1,'authoring::view::propertiesview::GameObjectProperties']]],
-  ['button_5ftext',['BUTTON_TEXT',['../de/d50/classerrors_and_exceptions_1_1_error_pop_up.html#a5acbe5fce488122627a7f6b53b5ac100',1,'errorsAndExceptions.ErrorPopUp.BUTTON_TEXT()'],['../de/dfc/classgame_player_1_1view_1_1keyboard_utility_1_1_key_map_form.html#ac0409eef2a5da9f8a536de58588973d1',1,'gamePlayer.view.keyboardUtility.KeyMapForm.BUTTON_TEXT()']]],
-  ['button_5fx_5fcoord',['BUTTON_X_COORD',['../de/d50/classerrors_and_exceptions_1_1_error_pop_up.html#a469410b12f5e1ba929814a1939f0317b',1,'errorsAndExceptions.ErrorPopUp.BUTTON_X_COORD()'],['../de/dfc/classgame_player_1_1view_1_1keyboard_utility_1_1_key_map_form.html#a2d1287723630d87163d5f70763a1b75f',1,'gamePlayer.view.keyboardUtility.KeyMapForm.BUTTON_X_COORD()']]],
-  ['button_5fy_5fcoord',['BUTTON_Y_COORD',['../de/d50/classerrors_and_exceptions_1_1_error_pop_up.html#a7a8a25ae1aad2f000a6d43ebfd371412',1,'errorsAndExceptions.ErrorPopUp.BUTTON_Y_COORD()'],['../de/dfc/classgame_player_1_1view_1_1keyboard_utility_1_1_key_map_form.html#af25d7c226932b9e0cf4874f7b9670282',1,'gamePlayer.view.keyboardUtility.KeyMapForm.BUTTON_Y_COORD()']]]
-];
